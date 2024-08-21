585ee9240583e2c0335a90dbf4bbf8aac8b1b4b0
pratiyush singh <pratiyush12@outlook.in>
2024-08-21 14:24:47 +0530

    addedplace holder

--- script.js
+++ script.js
@@ -11,48 +11,48 @@ toggleButton.addEventListener('click', () => {
 
 const data = [
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     ,
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
     {
-        img: "https://media.istockphoto.com/id/1324356458/vector/picture-icon-photo-frame-symbol-landscape-sign-photograph-gallery-logo-web-interface-and.jpg?s=612x612&w=0&k=20&c=ZmXO4mSgNDPzDRX-F8OKCfmMqqHpqMV6jiNi00Ye7rE=",
+        img: "https://placehold.co/700x400",
         name: "Project Name",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium obcaecati magnam debitis soluta deserunt tempore quasi sit possimus ad rerum ducimus dicta nulla impedit fuga molestias deleniti. Eligendi fuga"
     },
